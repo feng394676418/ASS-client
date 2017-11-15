9fa60ce2b04f0f4d4e5821bef0026a889683ee95
LiSyd <lixiaode621@163.com>
2017-11-15 16:21:58 +0800

    工单详情返回工单列表状态选择

--- src/views/order/index.vue
+++ src/views/order/index.vue
@@ -265,6 +265,9 @@
         }
   },
   created() {
+        this.$t('order.statusItems').forEach(function (item) {
+            item.active = false;
+　　　　});
         Array.prototype.remove = function (val) {
             var index = this.indexOf(val);
             if(index > -1){this.splice(index,1);}

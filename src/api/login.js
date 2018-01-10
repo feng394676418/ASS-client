91445e803d2361a9136443499a5f21072035f312
liuwy <731839038@qq.com>
2018-01-10 14:05:35 +0800

    update

--- src/api/login.js
+++ src/api/login.js
@@ -5,8 +5,9 @@ export function loginByEmail(username, password) {
         username,
         password,
         clientid: '3afddd82-846b-4445-9380-46df7db78c90', // 客户端ID
-        // authorizationuri: 'http://192.168.81.250:9966/authorize', // 校验授权码获取uri
-        authorizationuri: 'http://47.91.78.238:9966/authorize', // 生产授权码获取uri
+        authorizationuri: 'http://192.168.81.250:9966/authorize', // 校验授权码获取uri
+        // authorizationuri: 'http://localhost:9966/authorize', // 本地授权码获取uri
+        // authorizationuri: 'http://47.91.78.238:9966/authorize', // 生产授权码获取uri
         redirecturi: 'http://localhost:9901/' // 保留 暂未利用
     };
     return fetch({
@@ -28,10 +29,12 @@ export function getInfo(code, oauth_js_id, userName) {
     const oauth2AuthcInfo = { // oauth2配置
         clientId: '3afddd82-846b-4445-9380-46df7db78c90',
         clientSecret: '3223dbe57917b942f740630b230014c4',
-        // accessTokenUri: 'http://192.168.81.250:9966/accessToken', // 校验环境地址
-        // userInfoUri: 'http://192.168.81.250:9966/userInfo', // 校验环境地址
-        accessTokenUri: 'http://47.91.78.238:9966/accessToken', // 生产地址
-        userInfoUri: 'http://47.91.78.238:9966/userInfo', // 生产地址
+        accessTokenUri: 'http://192.168.81.250:9966/accessToken', // 校验环境地址
+        userInfoUri: 'http://192.168.81.250:9966/userInfo', // 校验环境地址
+        // accessTokenUri: 'http://localhost:9966/accessToken', // 本地环境地址
+        // userInfoUri: 'http://localhost:9966/userInfo', // 本地环境地址
+        // accessTokenUri: 'http://47.91.78.238:9966/accessToken', // 生产地址
+        // userInfoUri: 'http://47.91.78.238:9966/userInfo', // 生产地址
         redirectUri: 'http://localhost:9902/', // 保留 暂时未用
         no_redirect: 'true' // VUE 利用特别配置
     }
